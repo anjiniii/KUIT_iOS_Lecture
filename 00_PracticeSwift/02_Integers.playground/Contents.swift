17e25c30ea6efe357c93f06ccaccaf770de7467c
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-25 22:19:45 +0900

    [00_Swift]: 02_Integers

---
+++ 00_PracticeSwift/02_Integers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let age = 20
+let anjinAge = age + 6
+let halvedAge = age / 2
+
+var count = 10
+count += 5
+
+let number = 60
+/// 배수인지 파악
+print(number.isMultiple(of: 4))
+
+/// 범위 내에 있는 임의의 숫자
+let randomNumber = Int.random(in: 1...100)
