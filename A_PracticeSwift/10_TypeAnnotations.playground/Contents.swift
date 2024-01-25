894f5c44f7baa7174137dd96126bd9515dd2922b
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-25 22:47:58 +0900

    [A_Swift]: 10 Type Annotation

---
+++ A_PracticeSwift/10_TypeAnnotations.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let score: Double = 0
+
+let name: String = "가은"
+let age: Int = 20
+let pi: Double = 3.14
+var isEnabled: Bool = true
+
+var colors: [String] = ["red", "yellow", "green"]
+var user: [String: String] = ["id": "@hello"]
+var numbers: Set<Int> = Set([1, 7, 3, 8])
+
+var songs: [String] = [String]()
+var albums = [String]()
+
+enum Style {
+    case light, dark, system
+}
+
+var style: Style = .dark
