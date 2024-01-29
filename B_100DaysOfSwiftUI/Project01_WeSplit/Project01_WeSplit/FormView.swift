21ae2c22a078bbbc06823f803c428eaae45302b1
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-29 15:11:42 +0900

    B_SwiftUI_01: Form, Section

---
+++ B_100DaysOfSwiftUI/Project01_WeSplit/Project01_WeSplit/FormView.swift
@@ -0,0 +1,29 @@
+//
+//  FormView.swift
+//  Project01_WeSplit
+//
+//  Created by 이안진 on 1/29/24.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+        }
+    }
+}
+
+#Preview {
+    FormView()
+}
