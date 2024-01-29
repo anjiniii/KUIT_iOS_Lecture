2ca850218f42ea2b4559595de4be85551db84b89
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-29 15:57:21 +0900

    B_SwiftUI_01: WeSplitView tip 선택 picker, 총금액 추가

--- B_100DaysOfSwiftUI/Project01_WeSplit/Project01_WeSplit/WeSplitView.swift
+++ B_100DaysOfSwiftUI/Project01_WeSplit/Project01_WeSplit/WeSplitView.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct WeSplitView: View {
+    /// 입력 포커스를 관리하는 State
+    @FocusState private var amountIsFocused: Bool
+    
     /// @State를 통해 값이 변경되면 UI를 다시 로드
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmount: Double {
+        /// numberOfPeople은 picker의 index로 쓰고 있으므로 2명 추가 필요
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
     
     var totalPerPerson: Double {
         /// numberOfPeople은 picker의 index로 쓰고 있으므로 2명 추가 필요
@@ -47,11 +61,14 @@ struct WeSplitView: View {
                     /// 하드웨어 키보드가 있다면, 숫자 이외에도 입력될 수 있지만
                     /// return 키를 누르면 자동 필터링 된다
                     .keyboardType(.decimalPad)
+                    /// TextField에 포커스가 되어 있는지 확인
+                    .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         /// 처음 할당한 값이 2였는데 왜 기본값이 4 people인가?
                         /// 2, 3, 4, 5, ... 중에 index가 2인 값인 4가 나오는 것!
                         ForEach(2 ..< 100) {
+                            /// $0 - closure 사용!
                             Text("\($0) people")
                         }
                     }
@@ -68,13 +85,31 @@ struct WeSplitView: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ... 100, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
                 }
                 
-                Section {
+                Section("Tip 포함 총 금액") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("1인당 금액") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
+            /// toolbar - navigation bar, bottom, 등등
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
