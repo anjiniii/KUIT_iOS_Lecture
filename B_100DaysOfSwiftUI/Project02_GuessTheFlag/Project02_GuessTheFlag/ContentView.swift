89308248b495dbb384f1e2aff4ea15d349f38c4e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-29 16:01:09 +0900

    B_SwiftUI_02: 프로젝트 생성

---
+++ B_100DaysOfSwiftUI/Project02_GuessTheFlag/Project02_GuessTheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project02_GuessTheFlag
+//
+//  Created by 이안진 on 1/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ B_100DaysOfSwiftUI/Project02_GuessTheFlag/Project02_GuessTheFlag/Project02_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project02_GuessTheFlagApp.swift
+//  Project02_GuessTheFlag
+//
+//  Created by 이안진 on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Project02_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
