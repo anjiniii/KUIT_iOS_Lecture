6ed46a904ac7023b624a9f8ecfd54bd6b0df8ce0
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-03-04 23:31:21 +0900

    C_WeatherApp: 상단 날씨 정보 UI

--- C_Lecture/WeatherApp/WeatherApp/ContentView.swift
+++ C_Lecture/WeatherApp/WeatherApp/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(spacing: 0) {
+                Text("나의 위치")
+                    .font(.largeTitle)
+                
+                Text("포항시")
+                
+                Text(" 7º")
+                    .font(.system(size: 80))
+                    .fontWeight(.light)
+                
+                Text("흐림")
+                Text("최고:13º 최저:1º")
+            }
+            .padding(.vertical, 40)
+            
+            VStack(alignment: .leading) {
+                Text("내일, 더 낮은 기온 및 강우 상태가 예상됩니다.")
+                
+                Divider()
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        VStack(spacing: 8) {
+                            Text("지금")
+                            Image(systemName: "cloud.fill")
+                            Text("7º")
+                        }
+                        .font(.headline)
+                    }
+                }
+            }
+            .padding()
+            .background(Color.indigo.opacity(0.5))
+            .cornerRadius(12)
+            .padding(.horizontal, 20)
         }
-        .padding()
+        .background(Color.indigo.opacity(0.3))
     }
 }
 
