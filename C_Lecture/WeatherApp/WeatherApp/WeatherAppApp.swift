d0b916e47c50a0e9f342df94206934fee08222a7
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-03-04 23:19:00 +0900

    C_WeatherApp: 프로젝트 생성

---
+++ C_Lecture/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 이안진 on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
